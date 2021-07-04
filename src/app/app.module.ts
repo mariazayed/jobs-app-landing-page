98038dba124b117401d9a253bcd622fe2f61a155
Maria Zayed <m.zayed@bisan.com>
2021-07-04 10:02:25 +0300

    Added bootstrap + angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WavesComponent } from './waves/waves.component';
@@ -12,6 +12,7 @@ import { WavesComponent } from './waves/waves.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
